58496088b9b95457bf45a70eb33f4ee4f7320fa6
johnson <phantruongson96@gmail.com>
2021-03-21 12:44:35 -0400

    submit post and save to mongodb

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -2,14 +2,19 @@ import React, { useState } from 'react';
 import useStyles from './styles';
 import { TextField, Button, Typography, Paper } from '@material-ui/core';
 import FileBase from 'react-file-base64';
+import { useDispatch } from 'react-redux';
+import { createPost } from '../../actions/posts';
 
 
 const Form = () => {
   const [ postData, setPostData ] = useState({ creator: '', title: '', message: '', tags: '', selectedFile: '' })
   const classes = useStyles();
+  const dispatch = useDispatch();
 
-  const handleSubmit = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
+    dispatch(createPost(postData));
   }
 
   const clear = () => {
@@ -18,7 +23,7 @@ const Form = () => {
 
   return (
     <Paper className={classes.paper}>
-      <form autoComplete="off" noValidate className={classes.form} onSubmit={handleSubmit}>
+      <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
         <Typography variant="h6" >
           Creating a Memory
         </Typography>
